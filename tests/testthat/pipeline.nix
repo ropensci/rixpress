10f35cbc85b6b5aa150db48f61b60b068f270263
Bruno Rodrigues <bruno@brodrigues.co>
2025-03-07 23:30:52 +0100

    build pkgdown

---
+++ tests/testthat/pipeline.nix
@@ -0,0 +1,58 @@
+let
+  default = import ./default.nix;
+  pkgs = default.pkgs;
+  shell = default.shell;
+
+  commonBuildInputs = shell.buildInputs;
+  commonConfigurePhase = ''
+    cp ${./libraries.R} libraries.R
+    mkdir -p $out
+  '';
+
+  # Function to create R derivations
+  makeRDerivation = { name, buildPhase }:
+    let rdsFile = "${name}.rds";
+    in pkgs.stdenv.mkDerivation {
+      inherit name;
+      buildInputs = commonBuildInputs;
+      dontUnpack = true;
+      configurePhase = commonConfigurePhase;
+      inherit buildPhase;
+      installPhase = ''
+        cp ${rdsFile} $out/
+      '';
+    };
+
+  # Define all derivations
+  mtcars_am = makeRDerivation {
+    name = "mtcars_am";
+    buildPhase = ''
+      Rscript -e "
+        source('libraries.R')
+        mtcars_am <- dplyr::filter(mtcars, am == 1)
+        saveRDS(mtcars_am, 'mtcars_am.rds')"
+    '';
+  };
+
+  mtcars_head = makeRDerivation {
+    name = "mtcars_head";
+    buildPhase = ''
+      Rscript -e "
+        source('libraries.R')
+        mtcars_am <- readRDS('${mtcars_am}/mtcars_am.rds')
+        mtcars_head <- head(mtcars_am)
+        saveRDS(mtcars_head, 'mtcars_head.rds')"
+    '';
+  };
+
+  # Generic default target that builds all derivations
+  allDerivations = pkgs.symlinkJoin {
+    name = "all-derivations";
+    paths = with builtins; attrValues { inherit mtcars_am mtcars_head; };
+  };
+
+in
+{
+  inherit mtcars_am mtcars_head;  # Make individual derivations available as attributes
+  default = allDerivations;  # Set the default target to build everything
+}
