f1abc09bd75c255a45640851503000ffdb031b3f
Bruno Rodrigues <bruno@brodrigues.co>
2025-03-16 16:06:51 +0100

    Update gen_pipeline.R

--- data-raw/gen_pipeline.R
+++ data-raw/gen_pipeline.R
@@ -1,4 +1,5 @@
 library(rixpress)
+library(igraph)
 
 d0 <- rxp_file(
   mtcars,
@@ -22,5 +23,10 @@ rxp_list <- list(d0, d1, d2, d3, d4, doc)
 rixpress(rxp_list, project_path = ".")
 
 dag_obj <- plot_dag(return_igraph = TRUE)
+
+dag_obj <- set_vertex_attr(dag_obj, "label", value = V(dag_obj)$name)
+
+# Step 2: Delete the "name" attribute
+dag_obj <- delete_vertex_attr(dag_obj, "name")
 
 igraph::write_graph(dag_obj, file = "dag.dot", format = "dot")
