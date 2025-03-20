cea6a9a600c8b36eb0032428540754cc9a7999cb
Bruno Rodrigues <bruno@brodrigues.co>
2025-03-20 10:02:27 +0100

    Update rixpress.R

--- R/rixpress.R
+++ R/rixpress.R
@@ -69,9 +69,11 @@ rixpress <- function(derivs, project_path) {
   # Drop quarto objects, as these are handled separately
   nix_expressions_and_additional_files <- sapply(
       nix_expressions_and_additional_files,
-      function(x) if(x$type == "rxp_quarto"){
+      function(x) (if(x$type == "rxp_quarto"){
         x$additional_files <- ""
-      }
+      } else {
+        x$additional_files
+      })
     )
 
   flat_list <- list(
