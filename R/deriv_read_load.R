e9d89722f0f86f3a8b7491d0d7bb88d06fa5cc6f
Bruno Rodrigues <bruno@brodrigues.co>
2025-03-27 11:35:58 +0100

    better rxp_make

--- R/deriv_read_load.R
+++ R/deriv_read_load.R
@@ -3,56 +3,51 @@
 #'   match the given derivation name. If any matching file has a .pickle extension,
 #'   the function checks whether the reticulate package is installed.
 #' @param derivation_name The name of the derivation.
-#' @param result_path The folder containing the pipelines' outputs.
 #' @return A character vector of paths to the matching files.
 #' @noRd
 rxp_common <- function(derivation_name) {
-  build_log <- rxp_inspect_internal()
-
-  derivation <- subset(build_log, subset = derivation == derivation_name)
-
   # if derivation_name is a full path to the /nix/store simply return it
   # this is useful for Quarto documents
   if (grepl("^/nix/store/", derivation_name)) {
     return(derivation_name)
   }
-  files <- list.files(
-    Sys.readlink(list.files(
-      result_path,
-      pattern = "result",
-      full.names = TRUE
-    )),
-    full.names = TRUE
-  )
-  # Remove "all-derivations" from output, only needed to build everything at once
-  files <- files[!grepl("all-derivations", files)]
-  matching_files <- files[grepl(derivation_name, basename(files))]
-  if (length(matching_files) == 0) {
+
+  build_log <- rxp_inspect()
+
+  derivation <- subset(build_log, subset = derivation == derivation_name)
+
+  files <- unlist(derivation$output)
+
+  if (length(files) == 0) {
     stop(paste(
       "No derivation called",
       derivation_name,
-      "found. Did you build the pipeline?"
+      "found. Run `rxp_inspect()` to check if",
+      derivation_name,
+      "was built successfully."
     ))
   }
+
+  file_paths <- paste0(derivation$path, "/", files)
+
   # If any matching file is a .pickle, check if reticulate is installed
   if (
-    any(grepl(".*\\.pickle$", matching_files)) &&
+    any(grepl(".*\\.pickle$", file_paths)) &&
       !requireNamespace("reticulate", quietly = TRUE)
   ) {
     stop(
       "The 'reticulate' package is required to load .pickle files.\nPlease install it to use this feature."
     )
   }
-  matching_files
+  file_paths
 }
 
 #' @title Read output of a derivation
 #' @param derivation_name Character, the name of the derivation.
-#' @param result_path The folder containing the pipelines' outputs.
 #' @return The derivation's output.
 #' @export
-rxp_read <- function(derivation_name, result_path = "_rixpress") {
-  matching_files <- rxp_common(derivation_name, result_path = result_path)
+rxp_read <- function(derivation_name) {
+  matching_files <- rxp_common(derivation_name)
 
   # if there’s only one path, read it
   if (length(matching_files) == 1) {
@@ -71,12 +66,11 @@ rxp_read <- function(derivation_name, result_path = "_rixpress") {
 
 #' @title Load a derivation's output into the global environment
 #' @param derivation_name Character, the name of the derivation.
-#' @param result_path The folder containing the pipelines' outputs.
 #' @return None. The derivation object is assigned to the
 #'   global environment with the name `derivation_name`.
 #' @export
-rxp_load <- function(derivation_name, result_path = "_rixpress") {
-  matching_files <- rxp_common(derivation_name, result_path = result_path)
+rxp_load <- function(derivation_name) {
+  matching_files <- rxp_common(derivation_name)
   if (length(matching_files) == 1) {
     if (grepl(".*\\.rds$", matching_files)) {
       assign(derivation_name, readRDS(matching_files), envir = .GlobalEnv)
--- R/rxp_inspect.R
+++ R/rxp_inspect.R
@@ -1,45 +1,13 @@
-#' @noRd
-rxp_inspect_internal <- function() {
-  json_output <- system2(
-    "nix-instantiate",
-    args = c("--eval", "--strict", "--json", "pipeline.nix"),
-    stdout = TRUE
-  )
-
-  json_output <- paste(json_output, collapse = "")
-
-  build_paths_json <- jsonlite::fromJSON(json_output)
-
-  build_paths_df <- data.frame(
-    derivation = names(build_paths_json),
-    paths = unlist(build_paths_json, use.names = FALSE)
-  )
-
-  build_paths_df$output <- I(sapply(
-    build_paths_df$paths,
-    \(x) list(list.files(x, all.files = FALSE))
-  ))
-
-  build_paths_df$build_success <- sapply(
-    build_paths_df$output,
-    \(x) (!identical(x, character(0))),
-    USE.NAMES = FALSE
-  )
-
-  build_paths_df[, c("derivation", "build_success", "output", "paths")]
-}
-
-
-#' @title Inspect a buggy pipeline
-#' @description Returns a data frame with three columns:
+#' @title Inspect the build result of a pipeline.
+#' @description Returns a data frame with four columns:
 #'     - derivation: the name of the derivation
 #'     - build_success: whether the build was successful or not
-#'     - paths: the path of this derivation in the Nix store
+#'     - path: the path of this derivation in the Nix store
 #'     - output: the output, if this derivation was built successfully.
 #'               Empty outputs mean that this derivation was not built
-#'               successfully.
+#'               successfully. Several outputs for a single derivation
+#'               are possible.
 #'
-#' @importFrom jsonlite fromJSON
 #' @return A data frame with derivation names, if their build was successful,
 #'   their paths in the /nix/store, and their build outputs.
 #' @export
