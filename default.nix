8fbc58df57b51acd05d07806f818497bd0fe88a0
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-01 21:35:16 +0200

    updated dev env

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
- pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2025-03-17.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2025-03-31.tar.gz") {};
  
   rpkgs = builtins.attrValues {
     inherit (pkgs.rPackages) 
@@ -51,6 +51,7 @@ let
   
   system_packages = builtins.attrValues {
     inherit (pkgs) 
+      basedpyright
       glibcLocales
       glibcLocalesUtf8
       nix
