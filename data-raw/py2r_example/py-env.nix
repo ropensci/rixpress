9259fa5de771216a698c066174d6e9dadd3b7f7f
Bruno Rodrigues <bruno@brodrigues.co>
2025-05-08 13:58:15 +0200

    removed reticulate check, because it should not happen from the top execution env

--- data-raw/py2r_example/py-env.nix
+++ data-raw/py2r_example/py-env.nix
@@ -1,19 +1,50 @@
 let
  pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2025-03-17.tar.gz") {};
  
+  rpkgs = builtins.attrValues {
+    inherit (pkgs.rPackages) 
+      chronicler
+      dplyr
+      reticulate;
+  };
+
   pyconf = builtins.attrValues {
     inherit (pkgs.python312Packages) 
-      pandas
-      polars
-      pyarrow;
+      pandas;
   };
+
+    rix = (pkgs.rPackages.buildRPackage {
+      name = "rix";
+      src = pkgs.fetchgit {
+        url = "https://github.com/ropensci/rix/";
+        rev = "075a7001b5582a0cf7b0f5dfcbbfbb0672cd0b57";
+        sha256 = "sha256-6FJvSC5uD3h/LIkXcxV5d5uwXJXgbMxqzqDpQAccCXY=";
+      };
+      propagatedBuildInputs = builtins.attrValues {
+        inherit (pkgs.rPackages) 
+          codetools
+          curl
+          jsonlite
+          sys;
+      };
+    });
+
+  tex = (pkgs.texlive.combine {
+    inherit (pkgs.texlive) 
+      scheme-small
+      inconsolata;
+  });
 
   system_packages = builtins.attrValues {
     inherit (pkgs) 
       glibcLocales
       glibcLocalesUtf8
       nix
-      python312;
+      pandoc
+      python312
+      quarto
+      which
+      R;
   };
   
 shell = pkgs.mkShell {
@@ -25,7 +56,7 @@ shell = pkgs.mkShell {
    LC_PAPER = "en_US.UTF-8";
    LC_MEASUREMENT = "en_US.UTF-8";
 
-  buildInputs = [ pyconf system_packages ];
+  buildInputs = [ rix rpkgs pyconf tex system_packages ];
   
 };
 in
