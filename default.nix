195cb0fe88c40b2164f2ef1354abd81a4ed9ccaa
Bruno Rodrigues <bruno@brodrigues.co>
2025-08-21 15:27:51 +0200

    updated environment

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 let
- pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2025-07-07.tar.gz") {};
+ pkgs = import (fetchTarball "https://github.com/rstats-on-nix/nixpkgs/archive/2025-08-18.tar.gz") {};
  
   rpkgs = builtins.attrValues {
     inherit (pkgs.rPackages) 
